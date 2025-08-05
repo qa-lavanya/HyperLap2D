1b799cb0975464cb5e80caf81d6d3d6d28a30b1d
qa-lavanya <qa-lavanya@twentyideas.com>
2025-08-06 00:19:19 +0530

    dvxcxvc

---
+++ src/main/java/games/rednblack/editor/view/ui/followers/SubFollower.java
@@ -0,0 +1,40 @@
+package games.rednblack.editor.view.ui.followers;
+
+import com.badlogic.gdx.scenes.scene2d.Group;
+import games.rednblack.puremvc.interfaces.INotification;
+
+/**
+ * Created by CyberJoe on 7/2/2015.
+ */
+public abstract class SubFollower extends Group {
+
+    protected int entity;
+    protected BasicFollower parentFollower;
+
+    public SubFollower(int entity) {
+        setItem(entity);
+        create();
+    }
+
+    private void setItem(int entity) {
+        this.entity = entity;
+    }
+
+
+    public void handleNotification(INotification notification) {
+
+    }
+
+    public void setParentFollower(BasicFollower parent) {
+        this.parentFollower = parent;
+    }
+
+    @Override
+    public boolean remove() {
+        parentFollower = null;
+        return super.remove();
+    }
+
+    public abstract void create();
+    public abstract void update();
+}
